08b94f84a430ed9b6b339f99781fd6940f5a48fb
Ryo Igarashi <n33t5hin@gmail.com>
2022-12-21 07:04:55 +0900

    fix: Export form-data

--- browser.cjs
+++ browser.cjs
@@ -1,3 +1,5 @@
+"use strict";
+
 const Blob = globalThis.Blob;
 const FormData = globalThis.FormData;
 const Headers = globalThis.Headers;
@@ -5,11 +7,9 @@ const Request = globalThis.Request;
 const Response = globalThis.Response;
 const fetch = globalThis.fetch;
 
-module.exports = {
-  Blob,
-  FormData,
-  Headers,
-  Request,
-  Response,
-  fetch,
-};
+exports.Blob = Blob;
+exports.FormData = FormData;
+exports.Headers = Headers;
+exports.Request = Request;
+exports.Response = Response;
+exports.fetch = fetch;
--- index.cjs
+++ index.cjs
@@ -1,17 +1,19 @@
-const NodeFetch = require("node-fetch");
+"use strict";
 
-const Blob = globalThis.Blob || NodeFetch.Blob;
-const FormData = globalThis.FormData || NodeFetch.FormData;
-const Headers = globalThis.Headers || NodeFetch.Headers;
-const Request = globalThis.Request || NodeFetch.Request;
-const Response = globalThis.Response || NodeFetch.Response;
-const fetch = globalThis.fetch || NodeFetch.default;
+var NodeFetch = require("node-fetch");
+var NodeFormData = require("form-data");
+var buffer = require("buffer");
 
-module.exports = {
-  Blob,
-  FormData,
-  Headers,
-  Request,
-  Response,
-  fetch,
-};
+const Blob = globalThis.Blob ?? buffer.Blob;
+const FormData = globalThis.FormData ?? NodeFormData;
+const Headers = globalThis.Headers ?? NodeFetch.Headers;
+const Request = globalThis.Request ?? NodeFetch.Request;
+const Response = globalThis.Response ?? NodeFetch.Response;
+const fetch = globalThis.fetch ?? NodeFetch.default;
+
+exports.Blob = Blob;
+exports.FormData = FormData;
+exports.Headers = Headers;
+exports.Request = Request;
+exports.Response = Response;
+exports.fetch = fetch;
