9b1c4520c8115b6d6ef30117dcb47de01b5156d2
Ryo Igarashi <n33t5hin@gmail.com>
2023-02-04 14:42:10 +0900

    chore: Use undici types

--- index.d.ts
+++ index.d.ts
@@ -1,20 +1,23 @@
 export { Blob } from "node:buffer";
 export { AbortController, AbortSignal } from "abort-controller";
-export { default as FormData } from "form-data";
-export {
-  Body,
-  BodyInit,
-  default as fetch,
-  HeadersInit,
-  Headers,
-  Request,
-  RequestCache,
-  RequestContext,
-  RequestCredentials,
-  RequestInit,
-  RequestMode,
-  RequestRedirect,
-  Response,
-  ResponseInit,
-  ResponseType,
-} from "node-fetch";
+
+export type FormData = import("form-data") | import("undici/types/formdata").FormData;
+
+import * as NodeFetch from "node-fetch";
+import * as Undici from "undici/types/fetch";
+
+export type Body = NodeFetch.Body;
+export type BodyInit = NodeFetch.BodyInit | Undici.BodyInit;
+export type fetch = typeof NodeFetch.default | typeof Undici.fetch;
+export type HeadersInit = NodeFetch.HeaderInit | Undici.HeadersInit;
+export type Request = NodeFetch.Request | Undici.Request;
+export type RequestCache = NodeFetch.RequestCache | Undici.RequestCache;
+export type RequestContext = NodeFetch.RequestContext; // | Undici.RequestDestination;
+export type RequestCredentials = NodeFetch.RequestCredentials | Undici.RequestCredentials;
+export type RequestInfo = NodeFetch.RequestInfo | Undici.RequestInfo;
+export type RequestInit = NodeFetch.RequestInit | Undici.RequestInit;
+export type RequestMode = NodeFetch.RequestMode | Undici.RequestMode;
+export type RequestRedirect = NodeFetch.RequestRedirect | Undici.RequestRedirect;
+export type Response = NodeFetch.Response | Undici.Response;
+export type ResponseInit = NodeFetch.ResponseInit | Undici.ResponseInit;
+export type ResponseType = NodeFetch.ResponseType | Undici.ResponseType;
